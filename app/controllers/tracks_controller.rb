dcaebed27ef8fddefa38f7722da7402c69c4fe73
Alice Fresko <fresko.alice1@gmail.com>
2020-06-30 20:54:27 +0200

    create a new track

---
+++ app/controllers/tracks_controller.rb
@@ -0,0 +1,28 @@
+class TracksController < ApplicationController
+  def index
+    @tracks = Track.all
+  end
+
+  def new
+    @track = Track.new
+  end
+
+  def create
+    @track = Track.new(track_params)
+    @track.creator_id = current_user.id
+    if @track.save
+      redirect_to track_path(@track)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @track = Track.find(params[:id])
+  end
+
+  private
+  def track_params
+    params.require(:track).permit(:title, :description)
+  end
+end

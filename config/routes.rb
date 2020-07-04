eca024d43e83778a8891e7be314704adf6c636a3
victorgeai <victor.geai@gmail.com>
2020-07-04 17:12:08 +0200

    editor nav behavior ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tracks, only: [:index, :show, :new, :create] do
-    resources :chapters, only: [:index, :show, :new, :create]
+  resources :tracks, only: [:index, :show, :new, :create, :edit] do
+    resources :chapters, only: [:index, :show, :new, :create, :edit]
   end
   resources :subscriptions, only: [:create]
 end

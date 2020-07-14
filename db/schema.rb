b2a7844104ba7a7ca9213d6da1e0e2d05e35d4e0
Alice Fresko <fresko.alice1@gmail.com>
2020-07-14 22:54:43 +0200

    drag and drop chapters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_125904) do
+ActiveRecord::Schema.define(version: 2020_07_14_191342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_07_11_125904) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "rank"
+    t.integer "position"
     t.index ["track_id"], name: "index_chapters_on_track_id"
   end
 

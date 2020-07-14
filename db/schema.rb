9df50067e4db0a02ad93333ce4f3078a149c1001
Romain Coupey <schaft115@hotmail.com>
2020-07-14 20:42:05 +0200

    remove migration in double

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_125904) do
+ActiveRecord::Schema.define(version: 2020_07_11_080339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

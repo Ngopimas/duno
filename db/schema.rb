0ac9c61fe6a73c86b02690d11a506d2b3a13ee41
Romain Coupey <schaft115@hotmail.com>
2020-07-16 18:55:02 +0200

    fix smartphone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_080339) do
+ActiveRecord::Schema.define(version: 2020_07_14_191342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

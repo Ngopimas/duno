6a7c352884c4f2100ceb0e8d0504547c6d045209
Romain Coupey <schaft115@hotmail.com>
2020-07-21 20:48:18 +0200

    fix delete

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,6 +1,6 @@
 class Chapter < ApplicationRecord
   belongs_to :track
-  has_many :chapter_subscriptions
+  has_many :chapter_subscriptions, dependent: :destroy
   has_rich_text :content
   acts_as_list scope: :track
 end

3fe57d31b8672fa051d6190f1e21c41d58a536f9
Romain Coupey <schaft115@hotmail.com>
2020-07-25 10:10:16 +0200

    lvl logo update hot-fixed but.....beuuuuurk!

--- app/controllers/chapter_subscriptions_controller.rb
+++ app/controllers/chapter_subscriptions_controller.rb
@@ -5,6 +5,7 @@ class ChapterSubscriptionsController < ApplicationController
     p1 = current_user.subscriptions.find_by(track: @track).chapter_subscriptions.where(read: true).count.to_f
     p2 = @track.chapters.count.to_f
     @progress = (p1/p2*100) >= 0 ? (p1/p2*100).round : 0
+    console
     if @chapter_subscription.read == true
       @chapter_subscription.update(read: false)
     else
